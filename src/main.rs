941a65fb24b194aa269c5af807616e7ae67581ee
Sarguru02 <psgnhealth@gmail.com>
2024-01-09 23:44:51 +0530

    working v1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+use std::path::PathBuf;
 mod config;
 
 use config::parse_config;
@@ -41,5 +43,78 @@ fn logic(rule_path: Option<&str>, directory_to_organize: Option<&str>) {
     println!("Logic function"); 
     println!("Rule path {}", rule_path);
     println!("Directory to organize {}", directory_to_organize);
-    println!("Config {:#?}", config);
+
+    let paths = get_files(directory_to_organize).expect("Error getting files");
+
+    for i in config.rules {
+        println!("Rule name: {}", i.name);
+
+        create_directory(directory_to_organize, i.target_directory.as_str()).expect("Error creating directory");
+        for j in i.extensions {
+            let target_directory = format!("{}/{}", directory_to_organize, i.target_directory);
+            organize(&paths, j.as_str(), &target_directory).expect("Error printing files");
+        }
+    }
+    let paths = get_files(directory_to_organize).expect("Error getting files");
+    let target_directory = format!("{}/{}", directory_to_organize, "Others");
+    let _ = create_directory(directory_to_organize, "Others");
+    organize(&paths, "*", &target_directory).expect("Error printing files");
+
+}
+
+fn create_directory(base_path: &str, target_directory: &str) -> Result<(), std::io::Error>{
+    println!("Creating directory: {}/{}", base_path, target_directory);
+    let path = format!("{}/{}", base_path, target_directory);
+    fs::create_dir_all(&path)?;
+
+    Ok(())
+
+}
+
+fn get_files(directory: &str) -> Result<Vec<PathBuf>, std::io::Error> {
+
+        let dir_entries = fs::read_dir(directory)?;
+
+        let mut files = Vec::new();
+
+        for entry in dir_entries.filter_map(|entry| entry.ok()) {
+            if entry.file_type()?.is_file() {
+                files.push(entry.path());
+            }
+        }
+
+        Ok(files)
+
+}
+
+fn organize(paths: &Vec<PathBuf>, extension: &str, target_directory: &str) -> Result<(), std::io::Error>{
+    
+    if extension == "*" {
+        for path in paths {
+            let path_str = path.to_str().expect("Invalid file name");
+            move_file(path_str, target_directory)?;
+            println!("File: {:?}", path);
+        }
+        return Ok(())
+    }
+    for path in paths {
+        let path_str = path.to_str().expect("Invalid file name");
+        if let Some(file_extension) = path.extension().and_then(|e| e.to_str()) {
+            if file_extension == extension {
+                move_file(path_str, target_directory)?;
+                println!("File: {:?}", path);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+
+fn move_file(file_path: &str, target_directory: &str) -> Result<(), std::io::Error> {
+    let file_path = PathBuf::from(file_path);
+    let file_name = file_path.file_name().expect("Invalid file name");
+    let destination = format!("{}/{}", target_directory, file_name.to_str().unwrap());
+    fs::rename(file_path, destination)?;
+    Ok(())
 }
